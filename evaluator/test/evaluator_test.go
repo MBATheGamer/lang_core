0acffaeff102a6021198a67a935ab555e71c1ec7
MBATheGamer <mbathegamer@gmail.com>
2024-09-08 22:17:37 +0100

    Create `testEval` function

--- evaluator/test/evaluator_test.go
+++ evaluator/test/evaluator_test.go
@@ -1,1 +1,16 @@
 package evaluator_test
+
+import (
+	"github.com/MBATheGamer/lang_core/evaluator"
+	"github.com/MBATheGamer/lang_core/lexer"
+	"github.com/MBATheGamer/lang_core/object"
+	"github.com/MBATheGamer/lang_core/parser"
+)
+
+func testEval(input string) object.Object {
+	var lexer = lexer.New(input)
+	var parser = parser.New(lexer)
+	var program = parser.ParseProgram()
+
+	return evaluator.Eval(program)
+}
