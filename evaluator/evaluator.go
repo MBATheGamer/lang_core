98c9e5df5a6218bb1bc44bcb4935f3677052d40b
MBATheGamer <mbathegamer@gmail.com>
2024-09-28 13:18:55 +0100

    Add `evalBlockStatement` to `Eval` function

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -11,7 +11,7 @@ func Eval(node ast.Node) object.Object {
 		return evalProgram(node)
 
 	case *ast.BlockStatement:
-		return evalStatements(node.Statements)
+		return evalBlockStatement(node)
 
 	case *ast.IfExpression:
 		return evalIfExpression(node)
